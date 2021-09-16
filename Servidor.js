ac131c4b4b367678a87a9d55bf438d9cc33235d3
Eric Zaleta <48268776+MrUltraEnder@users.noreply.github.com>
2021-09-15 19:53:24 -0500

    json

--- Servidor.js
+++ Servidor.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var datos = [0,0,0,0,0,0,0,0,0];
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify(datos));
 }).listen(process.env.PORT); 
