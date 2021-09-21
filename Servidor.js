be86ace82b38bfc25836f7e2eb66ad9727aa15cf
Eric Zaleta <48268776+MrUltraEnder@users.noreply.github.com>
2021-09-20 19:21:12 -0500

    server

--- Servidor.js
+++ Servidor.js
@@ -4,4 +4,12 @@ var datos = [0,0,0,0,0,0,0,0,0];
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.end(JSON.stringify(datos));
+  if(req.url.lastIndexOf("juega")!==-1){
+  res.write("lets go");
+  }else
+  {
+    res.write("Game Over");
+  }
+  res.end();
+  }
 }).listen(process.env.PORT); 
